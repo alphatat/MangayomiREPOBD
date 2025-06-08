2d2b12f8196828e5aad5794fdbd7ac616daa8604
alphatat <fordrive2242@gmail.com>
2025-06-08 17:45:55 +0600

    Add files via upload

--- FibWatch/source.dart
+++ FibWatch/source.dart
@@ -3,14 +3,14 @@ import '../../../../../model/source.dart';
 Source get fibwatchSource => _fibwatchSource;
 const _fibwatchVersion = "0.0.1";
 const _fibwatchSourceCodeUrl =
-    "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/anime/src/en/fibwatch/fibwatch.dart";
+    "https://raw.githubusercontent.com/alphatat/MangayomiREPOBD/refs/heads/main/FibWatch/fibwatch.dart";
 Source _fibwatchSource = Source(
   name: "FibWatch",
   baseUrl: "https://fibwatch.art",
   lang: "bn",
   typeSource: "single",
   iconUrl:
-      "https://raw.githubusercontent.com/kodjodevf/mangayomi-extensions/$branchName/dart/anime/src/en/fibwatch/icon.png",
+      "https://raw.githubusercontent.com/alphatat/MangayomiREPOBD/refs/heads/main/FibWatch/icon.png",
   sourceCodeUrl: _fibwatchSourceCodeUrl,
   version: _fibwatchVersion,
   itemType: ItemType.anime,
