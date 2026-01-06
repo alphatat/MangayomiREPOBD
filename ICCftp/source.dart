3eb9547c2ccdcf50d1835762fb227fcd7576696b
alphatat <fordrive2242@gmail.com>
2026-01-07 01:50:24 +0600

    Add ICCFTP source configuration

--- ICCftp/source.dart
+++ ICCftp/source.dart
@@ -1,1 +1,17 @@
-test
+import '../../../../../model/source.dart';
+
+Source get iccftpSource => _iccftpSource;
+const _iccftpVersion = "0.0.1";
+const _iccftpSourceCodeUrl =
+    "https://raw.githubusercontent.com/alphatat/MangayomiREPOBD/refs/heads/main/ICCftp/iccftp.dart";
+Source _iccftpSource = Source(
+  name: "ICCFTP",
+  baseUrl: "http://10.16.100.244/",
+  lang: "bn",
+  typeSource: "single",
+  iconUrl:
+      "https://raw.githubusercontent.com/alphatat/MangayomiREPOBD/refs/heads/main/ICCftp/icon.png",
+  sourceCodeUrl: _iccftpSourceCodeUrl,
+  version: _iccftpVersion,
+  itemType: ItemType.anime,
+);
